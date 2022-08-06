63e231135cf844612568ae86906240fcac20368a
Steve Heindel <stevenheindel@gmail.com>
2022-08-05 22:26:42 -0400

    Improve error calculation

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,21 @@ impl Chunk {
         let pixel = avg;
 
         // Calculate error
-        let mut error: u64 = 0;
+        let mut mse = [0u64; 4];
         for (_x, _y, p) in sub.pixels() {
             // Make an extension trait to handle this?
             for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
-                error += u8::abs_diff(p[i], pixel[i]) as u64;
+                let err = u8::abs_diff(p[i], pixel[i]) as u64;
+                mse[i] += err * err;
             }
         }
+        for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
+            mse[i] /= sub.pixels().count() as u64;
+        }
+
+        //let coeffs = [0.2989, 0.5870, 0.1140, 0.0];
+        let coeffs = [1.0, 1.0, 1.0, 1.0];
+        let error: u64 = std::iter::zip(mse, coeffs).map(|(e, c)| (e as f64 * c) as u64).sum();
 
         Self {
             x,
@@ -56,6 +64,11 @@ impl Chunk {
         let x1 = self.x + width0;
         let y0 = self.y;
         let y1 = self.y + height0;
+
+        assert!(width0 > 1);
+        assert!(width1 > 1);
+        assert!(height0 > 1);
+        assert!(height1 > 1);
 
         #[rustfmt::skip]
         let chunks = [
@@ -85,7 +98,7 @@ fn main() {
         queue.sort_unstable_by_key(|c| c.error);
 
         count += 1;
-        if count >= 100 {
+        if count >= 500 {
             break;
         }
     }
