2e2619bfa0eef0f27e9889fa1f6f1ebf863a25f7
Steve Heindel <stevenheindel@gmail.com>
2022-09-29 12:59:56 -0400

    Some update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
-use image::{GenericImageView, Pixel, Rgba, ImageBuffer};
+use image::{GenericImageView, Pixel, Rgba, ImageBuffer, SubImage};
 
 type ImgRgba = ImageBuffer<Rgba<u8>, Vec<u8>>;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Filter {
+    Err,
+    SqErr,
+    Mse,
+}
 
 #[derive(Debug)]
 struct Chunk {
@@ -10,51 +17,35 @@ struct Chunk {
     height: u32,
     pixel: Rgba<u8>,
     error: u64,
+    filter: Filter,
 }
 
 impl Chunk {
-    fn from_img(img: &ImgRgba, x: u32, y: u32, width: u32, height: u32) -> Self {
-        println!("Chunk::from_img(x: {x}, y: {y}, width: {width}, height: {height}");
+    fn from_img(img: &ImgRgba, x: u32, y: u32, width: u32, height: u32, filter: Filter) -> Self {
+        //println!("Chunk::from_img(x: {x}, y: {y}, width: {width}, height: {height}");
         let sub = img.view(x, y, width, height);
 
-        // Filter the image data in the chunk's region
-        // There's gotta be a better way
-        let mut total = [0, 0, 0, 0];
-        for (_x, _y, p) in sub.pixels() {
-            for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
-                total[i] += p[i] as u32;
-            }
-        }
+        // Calculate raw error
+        let calc = match filter {
+            Filter::Err => todo!(),
+            Filter::SqErr => todo!(),
+            Filter::Mse => mse,
+        };
+        let (error_raw, pixel) = calc(&sub);
 
-        let mut avg = [0, 0, 0, 0];
-        for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
-            avg[i] = (total[i] / sub.pixels().count() as u32).try_into().unwrap();
-        }
-
-        // Calculate error
-        let mut mse = [0u64; 4];
-        for (_x, _y, p) in sub.pixels() {
-            // Make an extension trait to handle this?
-            for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
-                let err = u8::abs_diff(p[i], avg[i]) as u64;
-                mse[i] += err * err;
-            }
-        }
-        for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
-            mse[i] /= sub.pixels().count() as u64;
-        }
-
+        // Scale error based on color spectrum
         //let coeffs = [0.2989, 0.5870, 0.1140, 0.0];
-        let coeffs = [1.0, 1.0, 1.0, 1.0];
-        let error: u64 = std::iter::zip(mse, coeffs).map(|(e, c)| (e as f64 * c) as u64).sum();
+        let coeffs = [1.0, 1.0, 1.0, 0.0];
+        let error: u64 = std::iter::zip(error_raw, coeffs).map(|(e, c)| (e as f64 * c) as u64).sum();
 
         Self {
             x,
             y,
             width,
             height,
-            pixel: Rgba::from(avg),
+            pixel,
             error,
+            filter,
         }
     }
 
@@ -70,40 +61,36 @@ impl Chunk {
 
         if self.width > 1 && self.height > 1 {
             // Chunk is big enough to split into four
-            println!("Split 4");
             #[rustfmt::skip]
             let chunks = [
-                Some(Chunk::from_img(img, x0, y0, width0, height0)),
-                Some(Chunk::from_img(img, x1, y0, width1, height0)),
-                Some(Chunk::from_img(img, x0, y1, width0, height1)),
-                Some(Chunk::from_img(img, x1, y1, width1, height1)),
+                Some(Chunk::from_img(img, x0, y0, width0, height0, self.filter)),
+                Some(Chunk::from_img(img, x1, y0, width1, height0, self.filter)),
+                Some(Chunk::from_img(img, x0, y1, width0, height1, self.filter)),
+                Some(Chunk::from_img(img, x1, y1, width1, height1, self.filter)),
             ];
             chunks
         } else if self.width > 1 {
             // Chunk can only be split horizontally (vertical line)
-            println!("Split horiz");
             #[rustfmt::skip]
             let chunks = [
-                Some(Chunk::from_img(img, x0, y0, width0, self.height)),
-                Some(Chunk::from_img(img, x1, y0, width1, self.height)),
+                Some(Chunk::from_img(img, x0, y0, width0, self.height, self.filter)),
+                Some(Chunk::from_img(img, x1, y0, width1, self.height, self.filter)),
                 None,
                 None,
             ];
             chunks
         } else if self.height > 1 {
             // Chunk can only be split vertically (horizontal line)
-            println!("Split vert");
             #[rustfmt::skip]
             let chunks = [
-                Some(Chunk::from_img(img, x0, y0, self.width, height1)),
-                Some(Chunk::from_img(img, x0, y1, self.width, height1)),
+                Some(Chunk::from_img(img, x0, y0, self.width, height1, self.filter)),
+                Some(Chunk::from_img(img, x0, y1, self.width, height1, self.filter)),
                 None,
                 None,
             ];
             chunks
         } else {
             // Not sure if this would really happen
-            println!("Split none");
             [None, None, None, None]
         }
     }
@@ -115,21 +102,19 @@ pub struct Quad<'a> {
 }
 
 impl<'a> Quad<'a> {
-    pub fn from_img(img: &'a ImgRgba, depth: usize) -> Self {
-        let start = Chunk::from_img(&img, 0, 0, img.width(), img.height());
+    pub fn from_img(img: &'a ImgRgba, depth: usize, filter: Filter) -> Self {
+        let start = Chunk::from_img(&img, 0, 0, img.width(), img.height(), filter);
         let mut queue = vec![start];
         let mut count = 0;
         while let Some(chunk) = queue.pop() {
             // Get chunk with highest error
-            println!("Error: {}", chunk.error);
 
-            // Split chunk into four new chunks ()
+            // Split chunk into four new chunks (or fewer if unable to split)
             let chunks = chunk.split(img);
 
-            // Put each chunk back in the queue
+            // Put the new chunks into the queue. Keep the queue sorted.
             queue.extend(chunks.into_iter().filter_map(|x| x));
-            queue.sort_unstable_by_key(|c| c.error);
-            //dbg!(&queue);
+            queue.sort_unstable_by_key(|c| c.error); // TODO: use a BTreeSet?
 
             count += 1;
             if count >= depth {
@@ -143,8 +128,8 @@ impl<'a> Quad<'a> {
         }
     }
 
+    // Render each chunk into a new image
     pub fn render(&self, with_borders: bool) -> ImgRgba {
-        // Render each chunk into a new image
         let mut scratch = self.img.clone(); // TODO: clone img? or just create new one with same dims?
         for chunk in &self.chunks {
             let (x0, x1) = (chunk.x, chunk.x + chunk.width);
@@ -153,7 +138,7 @@ impl<'a> Quad<'a> {
             for y in y0..y1 {
                 for x in x0..x1 {
                     if with_borders == true && (x == x0 || x + 1 == x1 || y == y0 || y + 1 == y1) {
-                        scratch.put_pixel(x, y, Rgba::from([0, 0, 0, 0]));
+                        scratch.put_pixel(x, y, Rgba::from([0, 0, 0, 0])); // TODO: No alpha
                     } else {
                         scratch.put_pixel(x, y, chunk.pixel);
                     }
@@ -162,4 +147,35 @@ impl<'a> Quad<'a> {
         }
         scratch
     }
+}
+
+fn mse(sub: &SubImage<&ImgRgba>) -> ([u64; 4], Rgba<u8>) {
+    // Get average color
+    let mut total = [0, 0, 0, 0];
+    for (_x, _y, p) in sub.pixels() {
+        for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
+            total[i] += p[i] as u32;
+        }
+    }
+
+    let mut avg = [0, 0, 0, 0];
+    for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
+        avg[i] = (total[i] / sub.pixels().count() as u32).try_into().unwrap();
+    }
+
+    // Calculate error from average color
+    let mut mse = [0u64; 4];
+    for (_x, _y, p) in sub.pixels() {
+        for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
+            let err = u8::abs_diff(p[i], avg[i]) as u64;
+            mse[i] += err * err;
+        }
+    }
+
+    // MSE takes average of error
+    for i in 0..Rgba::<u8>::CHANNEL_COUNT as usize {
+        mse[i] /= sub.pixels().count() as u64;
+    }
+
+    (mse, Rgba::from(avg))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use image::io::Reader as ImageReader;
-use quadtree_image::Quad;
+use quadtree_image::{Quad, Filter};
 
 fn main() {
     let img = ImageReader::open("rock.jpg").unwrap().decode().unwrap();
     let img = img.to_rgba8(); // To keep this program simple we only operate in RGBA space
 
-    let quad = Quad::from_img(&img, 3); // TODO: let user specify depth
+    println!("Generating quadtree...");
+    let quad = Quad::from_img(&img, 500, Filter::Mse); // TODO: let user specify depth
+    println!("Rendering...");
     let output = quad.render(true);
 
+    println!("Save to disk...");
     output.save("output.png").unwrap();
 }
